44480766a97c706a422a11548364b4589d4a0229
Mirza <mirzaomerovic5@gmail.com>
2014-05-15 01:41:30 +0200

    Baza Klijent
    Dodao bazu klijent i klijent DAO

---
+++ Centar/Centar/KlijentDAO.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace Centar
+{
+    public class KlijentDAO:IDaoCrud<Klijent>
+    {
+        private MySqlConnection con;
+
+        public KlijentDAO(string host, string db, string user, string pass)
+        {
+            string connectionString = "server=localhost;user=" + user + ";pwd=" + pass + ";database=" + db;
+            con = new MySqlConnection(connectionString);
+            try
+            {
+                con.Open();
+            }
+            catch (Exception e) { throw e; }
+        }
+
+        public long create(Klijent entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Klijent read(Klijent entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Klijent update(Klijent entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void delete(Klijent entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Klijent getById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Klijent> getAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Klijent> getByExample(string name, string value)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
