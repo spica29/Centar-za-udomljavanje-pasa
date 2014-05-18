7945db33d824af0ce9bf02131c9f3d188501b2e5
spica29 <amela.spica@gmail.com>
2014-05-18 14:50:32 +0200

    add i edit radnika

---
+++ Centar/Centar/Add_Radnika.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace edit_i_dodavanje_radnika
+{
+    /// <summary>
+    /// Interaction logic for Add_Radnika.xaml
+    /// </summary>
+    public partial class Add_Radnika : Window
+    {
+        public Add_Radnika()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox_opisPosla_add_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (textBox_opisPosla_add.Text.ToLower() == "admin")
+            {
+                label_password_add.Visibility = System.Windows.Visibility.Visible;
+                label_username_add.Visibility = System.Windows.Visibility.Visible;
+                textBox_username_add.Visibility = System.Windows.Visibility.Visible;
+                pwBox_add.Visibility = System.Windows.Visibility.Visible;
+            }
+            else
+            {
+                label_password_add.Visibility = System.Windows.Visibility.Hidden;
+                label_username_add.Visibility = System.Windows.Visibility.Hidden;
+                textBox_username_add.Visibility = System.Windows.Visibility.Hidden;
+                pwBox_add.Visibility = System.Windows.Visibility.Hidden;
+            }
+        }
+
+        private void Window_Activated(object sender, EventArgs e)
+        {
+            label_password_add.Visibility = System.Windows.Visibility.Hidden;
+            label_username_add.Visibility = System.Windows.Visibility.Hidden;
+            textBox_username_add.Visibility = System.Windows.Visibility.Hidden;
+            pwBox_add.Visibility = System.Windows.Visibility.Hidden;
+        }
+    }
+}
